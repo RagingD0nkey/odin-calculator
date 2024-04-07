70aaf3ddefa98a3ebc88a2aaa7d4bade68f0de16
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-04-07 20:54:04 +0200

    Try new calculation logic

--- scripts.js
+++ scripts.js
@@ -1,72 +1,17 @@
 let prevNum = 0;
 let prevOperation ="";
 let result =0;
-let opFlag = true; //represents if last button clicked was an operator - initialized at true
-let equalFlag = false; //represents if last button clicked was the equal sign
 
 const btns = document.querySelector(".buttons");
 const display = document.querySelector(".display"); 
 
+display.textContent="0";
 
 btns.addEventListener("click", (e) => {
 
-    if (e.target.className ==="clearAll"){
-        prevNum = 0;
-        prevOperation ="";
-        result =0;
-        opFlag = true;
-        equalFlag=false;
-        updateDisplay("",true);
-    }
-
-    if (e.target.className ==="decimal"){
-
-    }
-
     if (e.target.className ==="digit"){
-        if  (opFlag===true){
-            updateDisplay(e.target.textContent,true)
-            opFlag = !opFlag;
-
-            if (equalFlag===true){
-                equalFlag=!equalFlag;
-            }
-        }
-        else {
-            updateDisplay(e.target.textContent);
-        }
-    }
-
-    else if (e.target.className ==="operator"){
-        if (opFlag===false){
-            if (prevOperation===""){
-                prevNum=Number(display.textContent);
-                prevOperation=e.target.id;
-                opFlag=true;
-            }
-            else {
-                result = operate(prevNum,Number(display.textContent),prevOperation);
-                display.textContent=result;
-                prevNum=result;
-                prevOperation=e.target.id;
-                opFlag=true;
-            }
-        }
-        else if (equalFlag===true){
-            prevNum=Number(display.textContent);
-            prevOperation=e.target.id;
-            equalFlag=!equalFlag;
-        }
-    }
-    
-    else if ((e.target.id ==="equalSign")&&(opFlag===false)&&(prevOperation!=="")){
-        result = operate(prevNum,Number(display.textContent),prevOperation);
-        display.textContent=result;
-        equalFlag=true;
-        opFlag=true;
-        prevNum="";
-        result=0;
-        prevOperation="";
+        console.log(Number.parseFloat(display.textContent)*10);
+        updateDisplay(add(Number.parseFloat(display.textContent)*10,Number.parseInt(e.target.textContent)));
     }
 
 })
@@ -97,7 +42,12 @@ let divide = function(a,b){
 
 function updateDisplay(newChar, replace = false){
     if (replace===false){
-        display.textContent+=newChar;
+        if (newChar.toString().length<6){
+            display.textContent=newChar;
+        }
+        else {
+            display.textContent=Number.parseFloat(newChar).toExponential(0);
+        }
     }
     else if (replace===true){
         display.textContent=newChar;
